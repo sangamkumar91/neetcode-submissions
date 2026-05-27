3d16b8b7aa51c9ecb3f6fabe9d7c95df576c15e3
Sangam Kumar <sangamkumar91@gmail.com>
2026-05-27 00:10:40 -0700

    Add: minimum-interval-including-query - submission-2

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] minInterval(int[][] intervals, int[] queries) {
+
+        List<int[]> indexedQuery = new ArrayList();
+        for (int i = 0; i < queries.length; i++) {
+            indexedQuery.add(new int[] {queries[i], i});
+        }
+
+        Collections.sort(indexedQuery, (a,b) -> a[0] - b[0]);
+        PriorityQueue<int[]> activeWindows = new PriorityQueue<>((a, b) -> {
+            return a[0] - b[0];
+        });
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        int i = 0;
+        int[] result = new int[queries.length];
+        Arrays.fill(result, -1);
+        for (int[] q : indexedQuery){
+            while (i < intervals.length && q[0] >= intervals[i][0]) {
+                activeWindows.offer(new int[] {
+                    (intervals[i][1] - intervals[i][0] + 1),
+                    intervals[i][1]
+                });
+                i++;
+            }
+            while (!activeWindows.isEmpty() && q[0] > activeWindows.peek()[1])
+                activeWindows.poll();
+            
+            if (!activeWindows.isEmpty()) {
+                result[q[1]] = activeWindows.peek()[0];
+            }
+        }
+
+
+        return result;
+    }
+}
