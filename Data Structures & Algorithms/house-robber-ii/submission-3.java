d47278012b038a48c59c0ab1f4344688b1f7d031
Sangam Kumar <sangamkumar91@gmail.com>
2026-05-27 12:29:29 -0700

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int[] memo1 = new int[nums.length];
+        int[] memo2 = new int[nums.length];
+        // As circluar, call dp twice once with first house and once with last house
+        return Math.max(
+            dp(nums, 1, nums.length, memo1), dp(nums, 0, nums.length - 1, memo2)
+        );
+    }
+
+    public int dp(int[] nums, int index, int end, int[] memo) {
+        if (index >= end) return 0;
+        int result = 0;
+        if (memo[index] != 0) return memo[index];
+        result = Math.max(
+            dp(nums, index + 1, end, memo), // House skipped
+            nums[index] + dp(nums, index + 2, end, memo) // House robbed
+            );
+
+        memo[index] = result;
+        return result;
+    }
+
+}
