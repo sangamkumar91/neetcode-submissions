a0b5f20df4679c27757eecaa56f6a129ed5ccb30
Sangam Kumar <sangamkumar91@gmail.com>
2026-05-27 16:38:33 -0700

    Add: reverse-integer - submission-4

---
+++ Data Structures & Algorithms/reverse-integer/submission-4.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        int num = 0;
+        while( x / 10 != 0 || x % 10 != 0) {
+            if (num > 0 && num > Integer.MAX_VALUE / 10) return 0;
+            if (num < 0 && num < Integer.MIN_VALUE / 10) return 0;
+            if (num > 0 && num * 10 > Integer.MAX_VALUE - x % 10) return 0;
+            if (num < 0 && num * 10 < Integer.MIN_VALUE - x % 10) return 0;
+            num = num * 10 + x % 10;
+            x = x / 10;
+        }
+        return num;
+    }
+}
