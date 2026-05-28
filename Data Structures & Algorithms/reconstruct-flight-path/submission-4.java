ce4e2a7d9edb04c9ffeeea3dfb0e2a42fcc28e18
Sangam Kumar <sangamkumar91@gmail.com>
2026-05-27 17:56:53 -0700

    Add: reconstruct-flight-path - submission-4

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-4.java
@@ -0,0 +1,49 @@
+class Dest {
+    String dst;
+    boolean visited;
+}
+class Solution {
+    List<String> result = new ArrayList();
+    int totalTickets;
+    public List<String> findItinerary(List<List<String>> tickets) {
+        totalTickets = tickets.size();
+        // Sort the tickets destimnation by lex order to make sure
+        // the first complete route itself is lex sorted
+        tickets.sort((a, b) -> a.get(1).compareTo(b.get(1)));
+        HashMap<String, List<Dest>> edges = new HashMap();
+        for (List<String> ticket : tickets) {
+            List<Dest> dests = edges.getOrDefault(ticket.get(0), new ArrayList());
+            Dest dest = new Dest();
+            dest.dst = ticket.get(1);
+            dest.visited = false;
+            dests.add(dest);
+            edges.put(ticket.get(0), dests);
+        }
+        dfs(edges, new ArrayList(), "JFK");
+        return result;
+    }
+
+    public boolean dfs(HashMap<String, List<Dest>> edges, List<List<String>> current, String src) {
+        if (current.size() == totalTickets) {
+            List<String> ports = current.stream().map(x -> x.get(0)).collect(Collectors.toList());
+            ports.add(current.get(current.size() - 1).get(1));
+            result = new ArrayList();
+            result.addAll(ports);
+            return true;
+        }
+
+        List<Dest> dests = edges.getOrDefault(src, new ArrayList());
+        for (Dest dest : dests) {
+            if (dest.visited) continue;
+            List<String> edge = new ArrayList();
+            edge.add(src);
+            edge.add(dest.dst);
+            dest.visited = true;
+            current.add(edge);
+            if (dfs(edges, current, dest.dst)) return true;
+            current.remove(current.size() - 1);
+            dest.visited = false;
+        }
+        return false;
+    }
+}
