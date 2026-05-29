810d7e040af8a9f734059769761227d8a6011845
Sangam Kumar <sangamkumar91@gmail.com>
2026-05-28 17:05:24 -0700

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int trap(int[] height) {
+
+        int curr = 0;
+        int total = 0;
+        int max = 0;
+        int maxHeight = 0;
+
+        // Left Valley
+        for (int i = 0; i < height.length; i++) {
+            int h = height[i];
+            maxHeight = Math.max(h, maxHeight);
+            if (h < max) {
+                curr = curr + (max - h);
+                continue;
+            }
+            total = total + curr;
+            curr = 0;
+            max = h;
+        }
+
+        // Right Valley till max height to not have water between two max heights counted twice;
+        curr = 0;
+        max = 0;
+        for (int i = height.length - 1; i >=0; i--) {
+            int h = height[i];
+            if (h < max) {
+                curr = curr + (max - h);
+                continue;
+            }
+            total = total + curr;
+            curr = 0;
+            max = h;
+            if (h == maxHeight) break;
+        }
+        return total;
+    }
+}
