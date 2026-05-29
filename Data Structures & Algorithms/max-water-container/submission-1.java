cdb0d81c791353c36a9ca3a077468fef6c637954
Sangam Kumar <sangamkumar91@gmail.com>
2026-05-28 17:31:40 -0700

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    // This is very different from Trap rain water
+    public int maxArea(int[] heights) {
+        int result = 0;
+        int left = 0;
+        int right = heights.length - 1;
+
+        while (left < right) {
+            int h1 = heights[left];
+            int h2 = heights[right];
+            result = Math.max(
+                result,
+                Math.min(h1, h2) * (right - left)
+            );
+            if (h1 < h2) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+        
+    }
+}
