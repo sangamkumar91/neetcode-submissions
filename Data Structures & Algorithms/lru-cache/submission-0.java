3c65e0e52d91d2c20cb571d3a81e99182803cecd
Sangam Kumar <sangamkumar91@gmail.com>
2026-06-01 13:24:52 -0700

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,88 @@
+class Node {
+    int val;
+    int key;
+    Node prev;
+    Node next;
+}
+class LRUCache {
+    HashMap<Integer, Node> store = new HashMap();
+    int cap;
+    int size = 0;
+    Node tail;
+    Node head;
+    public LRUCache(int capacity) {
+        cap = capacity;
+    }
+    public void detachNode(Node m) {
+        if (m == tail) {
+            tail = m.prev;
+            if (tail != null)
+                tail.next = null;
+        }
+        if (m == head) {
+            head = m.next;
+            if (head != null)
+                head.prev = null;
+        }
+        if (m != head && m != tail) {
+            Node prev = m.prev;
+            Node next = m.next;
+            if (prev != null)
+                prev.next = next;
+            if (next != null)
+                next.prev = prev;
+        }
+        m.prev = null;
+        m.next = null;
+    }
+
+    public void addNode(Node m) {
+        if (head == null) {
+            head = m;
+            tail = m;
+            return;
+        }
+
+        m.next = head;
+        head.prev = m;
+        head = m;
+    }
+
+    public int get(int key) {
+        System.out.println("get");
+
+        if (!store.containsKey(key)) return -1;
+        Node m = store.get(key);
+        System.out.println(store.toString());
+        detachNode(m);
+        addNode(m);
+        System.out.println("get");
+        return m.val;
+        
+    }
+    
+    public void put(int key, int value) {
+        System.out.println("put");
+        System.out.println(store.toString());
+
+        if (store.containsKey(key)) {
+            store.get(key).val = value;
+            this.get(key);
+            return;
+        }
+
+        Node m = new Node();
+        m.val = value;
+        m.key = key;
+        store.put(key, m);
+        addNode(m);
+        size++;
+
+        if (size > cap) {
+            Node x = tail;
+            store.remove(x.key);
+            detachNode(x);
+            size--;
+        }
+    }
+}
