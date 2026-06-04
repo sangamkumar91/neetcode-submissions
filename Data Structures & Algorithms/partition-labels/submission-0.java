bfb07774cc56d51663a8ceb1821329d8ada6f12b
Sangam Kumar <sangamkumar91@gmail.com>
2026-06-03 17:51:14 -0700

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+
+        HashMap<Character, int[]> intervals = new HashMap();
+
+        for (int i = 0 ; i < s.length(); i++) {
+            char c = s.charAt(i);
+            intervals.putIfAbsent(c, new int[] {i , i});
+            int[] interval = intervals.get(c);
+            interval[1] = i;
+        }
+
+        TreeSet<int[]> allIntervals = new TreeSet<>((a,b) -> {
+            return a[0] - b[0];
+        });
+        allIntervals.addAll(intervals.values());
+        List<int[]> result = new ArrayList();
+
+
+        for (int[] i: allIntervals) {
+            if (result.isEmpty()) {
+                result.add(i);
+                continue;
+            }
+            int size = result.size() - 1;
+            if (i[0] > result.get(size)[1]) {
+                result.add(i);
+                continue;
+            }
+            result.get(size)[1] = Math.max(result.get(size)[1], i[1]);
+        }
+        
+        return result.stream().map((a) -> a[1] - a[0] + 1).collect(Collectors.toList());
+        
+    }
+}
