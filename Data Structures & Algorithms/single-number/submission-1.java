4d64f6eed977efdcbc7ab60f385f0b04ddb65e53
Sangam Kumar <sangamkumar91@gmail.com>
2026-06-04 00:20:39 -0700

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                seen.remove(num);
+            } else {
+                seen.add(num);
+            }
+        }
+        return seen.iterator().next();
+    }
+}        
+
