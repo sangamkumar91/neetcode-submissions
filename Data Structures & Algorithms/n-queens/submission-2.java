2d5335741ed6f746dacecbc5f2b300ccde3cab94
Sangam Kumar <sangamkumar91@gmail.com>
2026-06-04 13:16:22 -0700

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.java
@@ -0,0 +1,64 @@
+class Solution {
+    int n;
+    char[][] view = null;
+    boolean[] rowFill;
+    boolean[] colFill;
+    boolean[] posDiagFill; // -> x + y = 0 || x + y = 1 ...  
+    boolean[] negDiagFill; // -> x - y = 0 || x - y = 1 ... 
+
+    List<List<String>> result = new ArrayList();
+    public List<List<String>> solveNQueens(int n) {
+        this.rowFill = new boolean[n];
+        this.colFill = new boolean[n];
+        this.posDiagFill = new boolean[2 * n];
+        this.negDiagFill = new boolean[2 * n];
+        this.n = n;
+        view = new char[n][n];
+        for (int i = 0; i < n ; i++)
+        for (int j = 0; j < n ; j++)
+            view[i][j] = '.';
+        dfs(0);
+        return result;
+    }
+    public void dfs(int q) {
+        if (q == n) {
+            List<String> combination = new ArrayList();
+            for (int i = 0; i < n ; i++) {
+                char[] row = view[i];
+                combination.add(new String(row));
+            }
+            result.add(combination);
+            return;
+        }
+        for (int i = 0; i < n ; i++) {
+            if (rowFill[i] || colFill[q] || posDiagFill[i + q] || negDiagFill[i - q + n]) continue;
+            placeQueen(i, q, q);
+            dfs(q + 1);
+            removeQueen(i, q, q);
+        }
+    }
+
+    public void placeQueen(int i, int j, int q) {
+        updateRows(i, q, true);
+        updateCols(j, q, true);
+        updateDiagonal(i, j, q, true);
+        view[i][j] = 'Q';
+    }
+    public void removeQueen(int i, int j, int q) {
+        updateRows(i, q, false);
+        updateCols(j, q, false);
+        updateDiagonal(i, j, q, false);        
+        view[i][j] = '.';
+    }
+
+    public void updateRows(int x, int q, boolean add) {
+        rowFill[x] = add;
+    }
+    public void updateCols(int y, int q, boolean add) {
+        colFill[y] = add;
+    }
+    public void updateDiagonal(int x, int y, int q, boolean add) {
+        posDiagFill[x + y] = add;
+        negDiagFill[x - y + n] = add;
+    }
+}
