0f12e00b548ae426a8802e1569664d3a797ebf76
Sangam Kumar <sangamkumar91@gmail.com>
2026-06-04 17:19:10 -0700

    Add: binary-tree-right-side-view - submission-4

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-4.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    List<Integer> res = new ArrayList();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        //return bfs(root);
+        dfs(root, 0);
+        return res;
+
+    }
+    public List<Integer> bfs(TreeNode root) {
+        List<Integer> result = new ArrayList();
+        if (root == null) return result;
+        ArrayDeque<TreeNode> queue = new ArrayDeque();
+        queue.offerFirst(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            Integer side = null;
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.pollLast();
+                side = current.val;
+                if (current.left != null) {
+                    queue.offerFirst(current.left);
+                }
+                if (current.right != null) {
+                    queue.offerFirst(current.right);
+                }
+            }
+            result.add(side);
+        }
+        return result;
+    }
+    public void dfs(TreeNode root, int depth) {
+        if (root == null) return;
+        // if depth already seen, dont add. As a righter node already exists.
+        if (res.size() == depth) {
+            res.add(root.val);
+        }
+        // Check right
+        dfs(root.right, depth + 1);
+        dfs(root.left, depth + 1);
+
+    }
+
+}
