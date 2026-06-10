1827bd8ab289692aee6d73ef3ead96f9078ca936
Sangam Kumar <sangamkumar91@gmail.com>
2026-06-09 22:47:56 -0700

    Add: subsets-ii - submission-7

---
+++ Data Structures & Algorithms/subsets-ii/submission-7.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        backtrack(new ArrayList(), nums, 0);
+        return res;
+    }
+
+    public void backtrack(List<Integer> current, int[] nums, int index) {
+        if (index == nums.length) {
+            res.add(new ArrayList(current));
+            return;
+        }
+
+        current.add(nums[index]);
+        backtrack(current, nums, index + 1);
+        current.remove(current.size() - 1);
+
+        index = index + 1;
+        while (index < nums.length && nums[index] == nums[index - 1]) index++;
+        backtrack(current, nums, index);
+    }
+}
